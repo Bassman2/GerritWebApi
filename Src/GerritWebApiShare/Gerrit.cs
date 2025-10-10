76e7bc5150ec239aee4066b42821e52bd9d550a2
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-10-10 13:00:46 +0200

    Update merge

--- Src/GerritWebApiShare/Gerrit.cs
+++ Src/GerritWebApiShare/Gerrit.cs
@@ -1,32 +1,24 @@
 ﻿namespace GerritWebApi;
 
-public sealed class Gerrit : JsonBaseClient
-{
-    private readonly GerritService? service;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Gerrit"/> class using a store key and application name.
-    /// </summary>
-    /// <param name="storeKey">The key to retrieve the host and token from the key store.</param>
-    /// <param name="appName">The name of the application.</param>
-    public Gerrit(string storeKey, string appName)
-        //        : this(new Uri(KeyStore.Key(storeKey)?.Host!), KeyStore.Key(storeKey)!.Token!, appName)
-        : this(new Uri(KeyStore.Key(storeKey)?.Host!), KeyStore.Key(storeKey)!.Login!, KeyStore.Key(storeKey)!.Password!, appName)
+public sealed class Gerrit : JsonService
+{
+    public Gerrit(string storeKey, string appName) : base(storeKey, appName, SourceGenerationContext.Default)
     { }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Gerrit"/> class with the specified host, token, and application name.
-    /// </summary>
-    /// <param name="host">The base URI of the Gerrit server.</param>
-    /// <param name="token">The authentication token for accessing the Gerrit API.</param>
-    /// <param name="appName">The name of the application.</param>
-    public Gerrit(Uri host, string token, string appName)
+    public Gerrit(Uri host, IAuthenticator? authenticator, string appName) : base(host, authenticator, appName, SourceGenerationContext.Default)
+    { }
+
+    protected override string? AuthenticationTestUrl => "a/config/server/version"; //"/access/api/v1/system/ping";
+
+
+    public override async Task<string?> GetVersionStringAsync(CancellationToken cancellationToken = default)
     {
-        service = DefineService(new GerritService(host, new BearerAuthenticator(token), appName));
+        WebServiceException.ThrowIfNotConnected(client);
+
+        var res = await GetStringAsync("a/config/server/version", cancellationToken);
+        res = res?.Trim(')', ']', '}', '\n', '"', '\'');
+        return res;
     }
 
-    public Gerrit(Uri host, string login, string password, string appName)
-    {
-        service = DefineService(new GerritService(host, new BasicAuthenticator("Authorization", login, password), appName));
-    }
 }
